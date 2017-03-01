36b385edaf7960669d6213673345c1854ece970e
aditya kumar <tigercartindia@gmail.com>
2017-03-01 07:46:29 +0000

    files has been added in jenkins deploy files

---
+++ test/server.js
@@ -0,0 +1,19 @@
+describe("Color Code Converter API", function() {
+
+  describe("RGB to Hex conversion", function() {
+
+    it("returns status 200", function() {});
+
+    it("returns the color in hex", function() {});
+
+  });
+
+  describe("Hex to RGB conversion", function() {
+
+    it("returns status 200", function() {});
+
+    it("returns the color in RGB", function() {});
+
+  });
+
+});
